44d9cdc6ea677b3a1f8b2863a6c2b73380f18b4e
Javad Ghanbarzadeh <Pouriya798@gmail.com>
2025-10-15 12:00:54 +0000

    مشکل اصلا از سایت نیست مشکل اینجاست که همیشه اینجا که چت میکردیم سمت چپ

--- next.config.ts
+++ next.config.ts
@@ -8,27 +8,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'no-store, max-age=0',
-          },
-          {
-            key: 'Pragma',
-            value: 'no-cache',
-          },
-          {
-            key: 'Expires',
-            value: '0',
-          },
-        ],
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
